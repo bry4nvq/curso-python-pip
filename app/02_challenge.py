16f071956654670dc98c21a1b999f347ca16e538
bry4nvq <brayanvalenciaocm@gmail.com>
2023-11-28 10:04:48 -0500

    28-11-2023 10:04

--- app/02_challenge.py
+++ app/02_challenge.py
@@ -1,10 +1,8 @@
 import read_csv as rd
 import charts
 
-path = './population.csv'
-population = rd.read_csv_uno(path)
-
 def countries_population_ratio(population, continent):
+
   countries_ratio = {country['Country/Territory']:country['World_Population_%'] for country in population if country['Continent'].lower() == continent.lower()}
   labels = list(countries_ratio.keys())
   values = list(countries_ratio.values())
@@ -12,11 +10,17 @@ def countries_population_ratio(population, continent):
   return labels, values
 
 def plot_pie_chat(population,continent):
+
   labels, values = countries_population_ratio(population, continent)
   charts.generate_pie_chart(continent, labels, values)
 
-#print(population[0])
-#print(countries_population_ratio(population))
-print("Type the continent to plot")
-continent = input("> ")
-plot_pie_chat(population, continent)
+def run():
+  path = './population.csv'
+  population = rd.read_csv_uno(path)
+  print("Type the continent to plot")
+  continent = input("> ")
+  plot_pie_chat(population, continent)
+
+if __name__ == '__main__':
+
+  run()
