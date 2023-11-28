acdde3be3becda61a7d59891ef349c2d493fc6b7
bry4nvq <brayanvalenciaocm@gmail.com>
2023-11-28 09:39:06 -0500

    28-11-2023 9:39

--- app/challenge.py
+++ app/challenge.py
@@ -1,5 +1,35 @@
 import read_csv as rd
 import charts
+#def run():
+  
+""" def select_country(countries, country):
+  
+  Select the country we want to plot it
+  
+  selected_country = filter(lambda item: item['Country/Territory'].lower() == country.lower(), countries)
+  selected_country = list(selected_country)[0]
+  
+  return selected_country
+
+def populations_country(country):
+  
+  filter the population by year columns of one dict
+  
+  populations = {year[:4]:population for (year,population) in country.items() if 'Population' in year and year[:4].isnumeric()}
+  return populations
+
+def plot_country(populations, country):
+  selected_country = select_country(populations, country)
+  populations_by_year = populations_country(selected_country)
+  labels = list(populations_by_year.keys())[::-1]
+  values = list(populations_by_year.values())[::-1]
+  charts.generate_bar_chart(country,labels,values)
+
+path = './population.csv'
+population = rd.read_csv_uno(path)
+print("Type the country to plot")
+country = input("> ")
+plot_country(population, country) """
 
 path = './population.csv'
 population = rd.read_csv_uno(path)
@@ -31,12 +61,13 @@ def plot_country(populations, country):
 #print(country)
 #print(populations)
 
-""" print("Type the country to plot")
+print("Type the country to plot")
 country = input("> ")
-plot = plot_country(population, country) """
+plot = plot_country(population, country)
 
-country = select_country(population, 'China')
-print({year[:4]:population for (year,population) in country.items() if 'Population' in year and year[:4].isnumeric()})
+#if __name__ == "__main__":
+ # run()
+
 
 
 
